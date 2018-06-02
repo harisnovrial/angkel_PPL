8ce87c762d4f9c154812fb775b594357e63bcd68
abdurahman <abdurahmanrizal1@gmail.com>
2018-06-02 14:07:57 +0700

    testing di menu hapus kerusakan

--- deleteKerusakan.php
+++ deleteKerusakan.php
@@ -3,7 +3,7 @@
     if(isset($_GET['id'])){
         $id = $_GET['id'];
 
-        $sql= "DELETE FROM pemesanan WHERE id_kerusakan = '$id'";
+        $sql= "DELETE FROM kerusakan WHERE id_kerusakan = '$id'";
         $query = mysqli_query($db,$sql);
 
         if($query){
