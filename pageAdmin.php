a2c8f7e48b5b571fc353b0f11a7aa231c2ee39ee
abdurahman <abdurahmanrizal1@gmail.com>
2018-06-06 17:34:18 +0700

    tambahan menampilkan history pemesanan dan history bengkel

--- pageAdmin.php
+++ pageAdmin.php
@@ -1,3 +1,6 @@
+<?php
+    include 'koneksi.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -52,17 +55,34 @@
                         <th>Jenis Kendaraan</th>
                         <th>Jumlah Kendaraan</th>
                         <th>Contact</th>
+                        <th>Status</th>
                         <th>Aksi</th>
                     </thead>
                     <tbody>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                       <?php
+                         $no = 1;
+                         $sql = "SELECT * FROM pemesanan ORDER BY tanggal";
+                         $query = mysqli_query($db,$sql);
+
+                         while($rowPemesanan = mysqli_fetch_array($query)){
+                             echo "<tr>";
+                             echo "<td>".$no."</td>";
+                             echo "<td>".$rowPemesanan['nama_pemesan']."</td>";
+                             echo "<td>".$rowPemesanan['lokasi_antar']."</td>";
+                             echo "<td>".$rowPemesanan['tanggal']."</td>";
+                             echo "<td>".$rowPemesanan['jenis_kendaraan']."</td>";
+                             echo "<td>".$rowPemesanan['jumlah_kendaraan']."</td>";
+                             echo "<td>".$rowPemesanan['cp']."</td>";
+                             echo "<td>".$rowPemesanan['status']."</td>";
+                             echo "<td>";
+                             echo '<a href="form-editPemesananAdmin.php?id='.$rowPemesanan['id_pemesanan'].'">Edit |</a>';
+                             echo '<a href="deletePemesanan.php?id='.$rowPemesanan['id_pemesanan'].'"> Hapus</a>';
+                            //  echo '<a href="cetakPemesanan.php?id='.$rowPemesanan['file'].'"> Cetak</a>';
+                             echo "</td>";
+                             echo "</tr>";
+                             $no++;
+                         }
+                       ?>
                     </tbody>
                 </table>
             </div>
@@ -73,23 +93,34 @@
                 <table class="responsive-table">
                     <thead>
                         <th>No</th>
-                        <th>Sopir</th>
+                        <th>NRP Sopir</th>
                         <th>No.Angkutan</th>
                         <th>Kerusakan</th>
                         <th>Keterangan</th>
                         <th>Tanggal</th>
+                        <th>Status</th>
                         <th>Nota</th>
-                        <th>Aksi</th>
                     </thead>
                     <tbody>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <?php
+                            $no = 1;
+                            $sqlBengkel = "SELECT * FROM kerusakan ORDER BY tanggal ASC";
+                            $queryBengkel = mysqli_query($db,$sqlBengkel);
+
+                            while($rowRusak = mysqli_fetch_array($queryBengkel)){
+                                echo "<tr>";
+                                echo "<td>".$no."</td>";
+                                echo "<td>".$rowRusak['NRP_sopir']."</td>";
+                                echo "<td>".$rowRusak['no_angkutan']."</td>";
+                                echo "<td>".$rowRusak['kerusakan']."</td>";
+                                echo "<td>".$rowRusak['keterangan']."</td>";
+                                echo "<td>".$rowRusak['tanggal']."</td>";
+                                echo "<td>".$rowRusak['status']."</td>";
+                                echo "<td>".$rowRusak['file']."</td>";
+                                echo "</tr>";
+                                $no++;
+                            }
+                        ?>
                     </tbody>
                 </table>
             </div>
